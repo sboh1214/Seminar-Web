c28f99a8a6f13d54ea431b13e475b49c606fd962
Seungbin Oh <sboh1214@gmail.com>
2021-06-06 00:48:19 +0900

    Add signup feature

---
+++ src/configs.ts
@@ -0,0 +1,49 @@
+import { UseToastOptions } from '@chakra-ui/toast'
+import axios, { AxiosResponse } from 'axios'
+
+export const API = axios.create({
+  baseURL: `http://localhost:8000/`,
+})
+
+export const StatusCode = {
+  Ok: 200,
+  Created: 201,
+  BadRequest: 400,
+  Unauthorized: 401,
+  Forbidden: 403,
+  InternalServerError: 500,
+  NotImplemented: 501,
+}
+
+type ToastFunction = (options?: UseToastOptions) => string | number
+
+export function toastServerError(toast: ToastFunction, data: any): void {
+  let message = 'No message from server.'
+  if (typeof data == 'string') {
+    message = data
+  }
+  toast({
+    title: 'Server Error',
+    description: message,
+    status: 'error',
+    duration: 5000,
+  })
+}
+
+export function toastInternetError(toast: ToastFunction): void {
+  toast({
+    title: 'Internet Error',
+    description: 'Please check your internet connection.',
+    status: 'error',
+    duration: 5000,
+  })
+}
+
+export function toastAxiosError(toast: ToastFunction, message: string): void {
+  toast({
+    title: 'Website Error',
+    description: message,
+    status: 'error',
+    duration: 5000,
+  })
+}
--- src/pages/user/signup.tsx
+++ src/pages/user/signup.tsx
@@ -1,4 +1,4 @@
-import { Container, Button } from '@chakra-ui/react'
+import { Container, Button, createStandaloneToast } from '@chakra-ui/react'
 import Frame from '../../components/frame'
 import {
   FormControl,
@@ -8,9 +8,12 @@ import {
 } from '@chakra-ui/react'
 import { useForm, useFormState } from 'react-hook-form'
 import { useRouter } from 'next/router'
+import { AxiosErrorToast, API, StatusCode, toastAxiosError, toastInternetError, toastServerError } from '../../configs'
+import axios, { AxiosError, AxiosResponse } from 'axios'
 
 export default function SignUp(): JSX.Element {
   const router = useRouter()
+  const toast = createStandaloneToast()
   const {
     control,
     register,
@@ -19,7 +22,53 @@ export default function SignUp(): JSX.Element {
   } = useForm()
   const { isSubmitting } = useFormState({ control })
   const onSubmit = (data) => {
-    router.push('/')
+    if (data.password != data.passwordCheck) {
+      toast({
+        title: 'Error',
+        description: "The passwords don't match.",
+        status: 'error',
+        duration: 5000,
+      })
+      return
+    }
+    API
+      .post(`auth/signup`, {
+        email: data.email,
+        password: data.password,
+        englishName: data.englishName,
+        koreanName: data.koreanName,
+      })
+      .then((res: AxiosResponse) => {
+        if (res.status == StatusCode.Created) {
+          toast({
+            title: 'Success',
+            description: "Your account created.",
+            status: 'success',
+            duration: 5000,
+          })
+          router.push(`/user/signin?email=${data.email}`)
+        }
+      })
+      .catch((err: AxiosError) => {
+        if (err.response) {
+          if (err.response.status == StatusCode.BadRequest) {
+            toast({
+              title: 'Error',
+              description: "Email already exists.",
+              status: 'error',
+              duration: 5000,
+            })
+          }
+          else if (err.response.status ==  StatusCode.InternalServerError) {
+            toastServerError(toast, err.response.data)
+          }
+        }
+        else if (err.request) {
+          toastInternetError(toast)
+        } else {
+          toastAxiosError(toast, err.message)
+        }
+      })
   }
 
   return (
