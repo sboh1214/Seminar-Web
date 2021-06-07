ee93151cd941a78afda8e944f5dca0f55893d706
Seungbin Oh <sboh1214@gmail.com>
2021-06-07 20:40:17 +0900

    Add links

--- src/components/card/seminarCard.tsx
+++ src/components/card/seminarCard.tsx
@@ -7,30 +7,33 @@ import {
   AccordionPanel,
   Text,
 } from '@chakra-ui/react'
+import Link from 'next/link'
 import UserCard from './userCard'
 
 export default function SeminarCard({ seminar }): JSX.Element {
   return (
-    <Box margin={3} padding={3} borderRadius={12} borderWidth={1}>
-      <Heading size="lg">{seminar.title}</Heading>
-      <Text marginY={3}>{seminar.description}</Text>
-      <Accordion allowMultiple>
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box flex="1" textAlign="left">
-                Speakers
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            {seminar.users?.map((user) => {
-              return <UserCard user={user} key={user.email} />
-            })}
-          </AccordionPanel>
-        </AccordionItem>
-      </Accordion>
-    </Box>
+    <Link href={`/seminar/${seminar.id}`}>
+      <Box margin={3} padding={3} borderRadius={12} borderWidth={1}>
+        <Heading size="lg">{seminar.title}</Heading>
+        <Text marginY={3}>{seminar.description}</Text>
+        <Accordion allowMultiple>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box flex="1" textAlign="left">
+                  Speakers
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              {seminar.users?.map((user) => {
+                return <UserCard user={user} key={user.email} />
+              })}
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </Box>
+    </Link>
   )
 }
--- src/components/card/seriesCard.tsx
+++ src/components/card/seriesCard.tsx
@@ -7,47 +7,50 @@ import {
   AccordionPanel,
   Text,
 } from '@chakra-ui/react'
+import Link from 'next/link'
 
 export default function SeriesCard({ series }): JSX.Element {
   return (
-    <Box margin={3} padding={3} borderRadius={12} borderWidth={1}>
-      <Text>{series.title}</Text>
-      <Text>{series.description}</Text>
-      <Accordion allowMultiple>
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box flex="1" textAlign="left">
-                Speakers
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </AccordionPanel>
-        </AccordionItem>
+    <Link href={`/series/${series.id}`}>
+      <Box margin={3} padding={3} borderRadius={12} borderWidth={1}>
+        <Text>{series.title}</Text>
+        <Text>{series.description}</Text>
+        <Accordion allowMultiple>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box flex="1" textAlign="left">
+                  Speakers
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </AccordionPanel>
+          </AccordionItem>
 
-        <AccordionItem>
-          <h2>
-            <AccordionButton>
-              <Box flex="1" textAlign="left">
-                Seminars
-              </Box>
-              <AccordionIcon />
-            </AccordionButton>
-          </h2>
-          <AccordionPanel pb={4}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </AccordionPanel>
-        </AccordionItem>
-      </Accordion>
-    </Box>
+          <AccordionItem>
+            <h2>
+              <AccordionButton>
+                <Box flex="1" textAlign="left">
+                  Seminars
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </Box>
+    </Link>
   )
 }
