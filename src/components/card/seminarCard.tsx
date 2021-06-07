4a3253499de94fcf335c4b36413ea85f49cd0e89
Seungbin Oh <sboh1214@gmail.com>
2021-06-07 20:41:31 +0900

    Fix range of click

--- src/components/card/seminarCard.tsx
+++ src/components/card/seminarCard.tsx
@@ -12,28 +12,28 @@ import UserCard from './userCard'
 
 export default function SeminarCard({ seminar }): JSX.Element {
   return (
-    <Link href={`/seminar/${seminar.id}`}>
-      <Box margin={3} padding={3} borderRadius={12} borderWidth={1}>
+    <Box margin={3} padding={3} borderRadius={12} borderWidth={1}>
+      <Link href={`/seminar/${seminar.id}`}>
         <Heading size="lg">{seminar.title}</Heading>
         <Text marginY={3}>{seminar.description}</Text>
-        <Accordion allowMultiple>
-          <AccordionItem>
-            <h2>
-              <AccordionButton>
-                <Box flex="1" textAlign="left">
-                  Speakers
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>
-              {seminar.users?.map((user) => {
-                return <UserCard user={user} key={user.email} />
-              })}
-            </AccordionPanel>
-          </AccordionItem>
-        </Accordion>
-      </Box>
-    </Link>
+      </Link>
+      <Accordion allowMultiple>
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box flex="1" textAlign="left">
+                Speakers
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            {seminar.users?.map((user) => {
+              return <UserCard user={user} key={user.email} />
+            })}
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+    </Box>
   )
 }
--- src/components/card/seriesCard.tsx
+++ src/components/card/seriesCard.tsx
@@ -11,46 +11,46 @@ import Link from 'next/link'
 
 export default function SeriesCard({ series }): JSX.Element {
   return (
-    <Link href={`/series/${series.id}`}>
-      <Box margin={3} padding={3} borderRadius={12} borderWidth={1}>
+    <Box margin={3} padding={3} borderRadius={12} borderWidth={1}>
+      <Link href={`/series/${series.id}`}>
         <Text>{series.title}</Text>
         <Text>{series.description}</Text>
-        <Accordion allowMultiple>
-          <AccordionItem>
-            <h2>
-              <AccordionButton>
-                <Box flex="1" textAlign="left">
-                  Speakers
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </AccordionPanel>
-          </AccordionItem>
+      </Link>
+      <Accordion allowMultiple>
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box flex="1" textAlign="left">
+                Speakers
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </AccordionPanel>
+        </AccordionItem>
 
-          <AccordionItem>
-            <h2>
-              <AccordionButton>
-                <Box flex="1" textAlign="left">
-                  Seminars
-                </Box>
-                <AccordionIcon />
-              </AccordionButton>
-            </h2>
-            <AccordionPanel pb={4}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
-            </AccordionPanel>
-          </AccordionItem>
-        </Accordion>
-      </Box>
-    </Link>
+        <AccordionItem>
+          <h2>
+            <AccordionButton>
+              <Box flex="1" textAlign="left">
+                Seminars
+              </Box>
+              <AccordionIcon />
+            </AccordionButton>
+          </h2>
+          <AccordionPanel pb={4}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+    </Box>
   )
 }
